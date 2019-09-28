75ab6a361f312e2fcdd7f22c2ce3e17e7594de8b
Nat Alison <tesseralis@gmail.com>
2019-09-27 17:13:54 -0700

    mispellings are cute

--- src/pages/page-4.js
+++ src/pages/page-4.js
@@ -9,7 +9,7 @@ const FourthPage = () => (
     <SEO title="Page Four" />
     <h1>Hi from the fourth page</h1>
     <p>I lied.</p>
-    <p>I bamboozled you.</p>
+    <p>I bamboozzled you.</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
